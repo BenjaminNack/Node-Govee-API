fe5723f061ff195c375d448b0a06833e3ca65c2a
Benjamin Nack <ben@nackfamily.com>
2020-12-07 17:08:35 -0600

    Setup basic request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
 const request = require("request");
 const models = require('./deviceModels');
+
+let apikey = '';
+let device = '';
+let model = '';
+
+function initDevice(key, macaddress, model){
+    apikey = key;
+    device = macaddress;
+    model = model;
+}
+
+const control = {
+    setOn: function(on) {
+        request('https://developer-api.govee.com/v1/devices/control', 
+            { 
+                ethod: 'PUT', 
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Govee-API-Key': apikey
+                },
+                formData: {
+                    device,
+                    model,
+                    cmd: {
+                        name: "turn",
+                        value: on ? "on" : "off",
+                    }
+                }
+            }
+        );
+    }
+}
+
+export default {
+    initDevice,
+    models,
+}
