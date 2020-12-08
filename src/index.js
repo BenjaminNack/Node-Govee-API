e0e240f86c683b9afbc2409b1bb4a33aa200a206
Benjamin Nack <ben@nackfamily.com>
2020-12-07 18:21:00 -0600

    Make setOn function async and fix model definition

--- src/index.js
+++ src/index.js
@@ -5,28 +5,36 @@ let apikey = '';
 let device = '';
 let model = '';
 
-function initDevice(key, macaddress, model){
+function initDevice(key, macaddress, deviceModel){
     apikey = key;
     device = macaddress;
-    model = model;
+    model = deviceModel;
 }
 
 const control = {
     setOn: function(on) {
-        superagent
-            .put('https://developer-api.govee.com/v1/devices/control')
-            .send({  
+        return new Promise(function(resolve, reject) {
+            const data = {  
                 device: device,
                 model: model,
                 cmd: {
                     name: "turn",
                     value: on ? "on" : "off",
                 }
-            })
-            .set('Govee-API-Key', apikey)
-            .end((err, res) => {
-                console.log(err);
-            });
+            };
+    
+            superagent
+                .put('https://developer-api.govee.com/v1/devices/control')
+                .send(data)
+                .set('Govee-API-Key', apikey)
+                .end(function (err, res){
+                    if(!err){
+                        resolve(res);
+                    }
+
+                    reject(err);
+                });
+        });
     }
 }
 
